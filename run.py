7bed9edc8d8df9614e05d5a4063326a488a491f1
patthoege <paty.hoge98@gmail.com>
2023-07-20 16:31:59 +0200

    Add word.py file for the words and add start section

--- run.py
+++ run.py
@@ -1,11 +1,22 @@
-"""
-Import section
-"""
+#import section
 import random
 import time
 import game_ascii_art
 import game_effects
+from words import word_list
 
-# constant with a list of words options for the game
-game_words = ["oxygen", "microwave", "mystify", "galaxy",
-               "syndrome", "subway", "voodoo", "peekaboo", "matrix"]
+#Start section
+print("Welcome to Hangman!")
+user_name = input("Enter your name:")
+print("Hi " + user_name + "! Time to play! =D")
+time.sleep(1)
+print("Start guessing...\n")
+time.sleep(0.5)
+
+
+def get_random_word():
+    """
+    Function to select a random word from the list and return the letter in uppercase.
+    """
+    word = random.choice(word_list)
+    return word.upper()
